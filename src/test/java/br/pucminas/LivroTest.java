061259d9e27a3fe38667478667fe0a5c6452d18f
Marlene Moraes <marlenevmoraes@outlook.com>
2023-09-04 19:38:30 -0300

    code: add testRetornarLivro() test

--- src/test/java/br/pucminas/LivroTest.java
+++ src/test/java/br/pucminas/LivroTest.java
@@ -1,28 +1,38 @@
 package test.java.br.pucminas;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import main.java.br.pucminas.Livro;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class LivroTest {
 
-    @Test
-    public void testCriarLivro(){
-        Livro livro = new Livro("Java", "Deitel", 1, false);
-        assertEquals("Java", livro.getTitulo());
-        assertEquals("Deitel", livro.getAutor());
-        assertEquals(1, livro.getId());
-        assertFalse(livro.isEmprestado());
-    }
+	@Test
+	public void testCriarLivro() {
+		Livro livro = new Livro("Java", "Deitel", 1, false);
+		assertEquals("Java", livro.getTitulo());
+		assertEquals("Deitel", livro.getAutor());
+		assertEquals(1, livro.getId());
+		assertFalse(livro.isEmprestado());
+	}
 
-    @Test
-    public void testEmprestarLivro(){
-        Livro livro = new Livro("Java", "Deitel", 1, false);
-        assertFalse(livro.isEmprestado());
-        livro.setEmprestado(true);
-        assertTrue(livro.isEmprestado());
-    }
+	@Test
+	public void testEmprestarLivro() {
+		Livro livro = new Livro("Java", "Deitel", 1, false);
+		assertFalse(livro.isEmprestado());
+		livro.setEmprestado(true);
+		assertTrue(livro.isEmprestado());
+	}
 
+	@Test
+	public void testRetornarLivro() {
+		Livro livro = new Livro("Java", "Deitel", 1, true);
+		assertTrue(livro.isEmprestado());
+
+		livro.setEmprestado(false);
+		assertFalse(livro.isEmprestado());
+	}
 }
