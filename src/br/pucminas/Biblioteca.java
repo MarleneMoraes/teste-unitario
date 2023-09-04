68b343536ebafb791239e11ce740e9a067b672b5
Marlene Moraes <marlenevmoraes@outlook.com>
2023-09-04 19:02:11 -0300

    code: refact object classes

--- src/br/pucminas/Biblioteca.java
+++ src/br/pucminas/Biblioteca.java
@@ -5,4 +5,70 @@ import java.util.List;
 public class Biblioteca {
 	private List<Livro> acervo;
 	private List<Membro> membros;
+
+	public void registrarLivro(Livro livro) {
+    try {
+			if (!livroExiste(livro)) {
+				acervo.add(livro);
+			}
+		} catch (Exception e) {
+			e.getMessage();
+		}
+	}
+
+	public void emprestarLivro(int livroId, int membroId) {
+		try {
+			Livro livro = validaLivroId(livroId);
+			Membro membro = validaMembroId(membroId);
+			
+			if (livro != null &&  membro != null) {
+				livro.setEmprestado(true);
+				membro.pegarEmprestado(livro);
+			}
+		} catch (Exception e) {
+			e.getMessage();
+		}
+
+	}
+
+	public void retornarLivro(int livroId, int membroId) {
+		try {
+			Livro livro = validaLivroId(livroId);
+			Membro membro = validaMembroId(membroId);
+			
+			if (livro != null &&  membro != null) {
+				livro.setEmprestado(false);
+				membro.retornarLivro(livro);
+			}
+		} catch (Exception e) {
+			e.getMessage();
+		}
+	}
+
+	private Livro validaLivroId(int livroId) {
+		for (Livro livro : acervo) {
+			if (livro.getId() == livroId) {
+				return livro;
+			}
+		}
+		return null;
+	}
+
+	private Membro validaMembroId(int membroId) {
+		for (Membro membro : membros) {
+			if (membro.getId() == membroId) {
+				return membro;
+			}
+		}
+		return null;
+	}
+
+  private boolean livroExiste(Livro livro){
+    for (Livro livroAcervo : acervo) {
+			if (livroAcervo.getTitulo() == livro.getTitulo()) {
+				return true;
+			}
+		}
+		return false;
+  }
 }
--- src/br/pucminas/Membro.java
+++ src/br/pucminas/Membro.java
@@ -4,43 +4,64 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Membro {
-	private int id;
-	private String nome;
-	private List<Livro> livrosEmprestados;
+  private int id;
+  private String nome;
+  private List<Livro> livrosEmprestados;
 
-	public Membro(int id, String nome) {
-		this.id = id;
-		this.nome = nome;
-		this.livrosEmprestados = new ArrayList<>();
-	}
+  public Membro(int id, String nome) {
+    this.id = id;
+    this.nome = nome;
+    this.livrosEmprestados = new ArrayList<>();
+  }
 
-	public Membro(int id, String nome, List<Livro> livrosEmprestados) {
-		this.id = id;
-		this.nome = nome;
-		this.livrosEmprestados = livrosEmprestados;
-	}
+  public Membro(int id, String nome, List<Livro> livrosEmprestados) {
+    this.id = id;
+    this.nome = nome;
+    this.livrosEmprestados = livrosEmprestados;
+  }
 
-	public int getId() {
-		return id;
-	}
+  public int getId() {
+    return id;
+  }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+  public void setId(int id) {
+    this.id = id;
+  }
 
-	public String getNome() {
-		return nome;
-	}
+  public String getNome() {
+    return nome;
+  }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
-	public List<Livro> getLivrosEmprestados() {
-		return livrosEmprestados;
-	}
+  public List<Livro> getLivrosEmprestados() {
+    return livrosEmprestados;
+  }
 
-	public void setLivrosEmprestados(List<Livro> livrosEmprestados) {
-		this.livrosEmprestados = livrosEmprestados;
-	}
+  public void setLivrosEmprestados(List<Livro> livrosEmprestados) {
+    this.livrosEmprestados = livrosEmprestados;
+  }
+
+  public void pegarEmprestado(Livro livro) {
+    try {
+      livrosEmprestados.add(livro);
+    } catch (Exception e) {
+      e.getMessage();
+    }
+  }
+
+  public void retornarLivro(Livro livro) {
+    try {
+      for (Livro livroEmprestado : livrosEmprestados) {
+        if (livro.equals(livroEmprestado)) {
+          livrosEmprestados.remove(livro);
+          break;
+        }
+      }
+    } catch (Exception e) {
+      e.getMessage();
+    }
+  }
 }
